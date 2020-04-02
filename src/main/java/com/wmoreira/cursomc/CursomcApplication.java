3b735901a6104a27bf09f628809fe6a82617cc76
root <wsm1986@gmail.com>
2020-04-02 19:41:02 -0300

    token

--- src/main/java/com/wmoreira/cursomc/CursomcApplication.java
+++ src/main/java/com/wmoreira/cursomc/CursomcApplication.java
@@ -1,14 +1,28 @@
 package com.wmoreira.cursomc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.wmoreira.cursomc.domain.Cliente;
+import com.wmoreira.cursomc.domain.enums.TipoCliente;
+import com.wmoreira.cursomc.repository.CategoriaRepository;
+import com.wmoreira.cursomc.repository.CidadeRepository;
+import com.wmoreira.cursomc.repository.ClienteRepository;
+import com.wmoreira.cursomc.repository.EstadoRepository;
+import com.wmoreira.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,5 +30,10 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {		
+		
+		Cliente cli1 = new Cliente(null, "Well", "wsm@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		
+		clienteRepository.save(cli1);
+
 	}	
 }
